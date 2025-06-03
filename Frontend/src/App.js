f57d0a1defa0933f877dbf14a0402d5c9730cb43
spiritualsquad <93115878+spiritualsquad@users.noreply.github.com>
2025-06-02 21:25:37 -0400

    lil integration

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -13,11 +13,29 @@ function App() {
   }, []);
 
   return (
+    <>
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>{message}</h1>
       <h1>It works</h1>
+      
     </div>
+    <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
   );
 }
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
 
 export default App;
